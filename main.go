6ad06b42c046cb460fd481f3ae66ef4717c40fa8
syunsuke-I <odol@ymail.ne.jp>
2023-12-14 20:41:16 +0900

    DB接続まで

--- main.go
+++ main.go
@@ -5,13 +5,28 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+var DB *gorm.DB
+
+func connectionDatabase() {
+	database, err := gorm.Open(mysql.Open("notes:tmp_pwd@tcp(127.0.0.1:3306)/notes?charset=utf8"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connected to database!")
+	}
+
+	DB = database
+}
 
 func main() {
 	r := gin.Default()
 	r.Use(gin.Logger())
 
 	r.LoadHTMLGlob("templates/**/**")
+
+	connectionDatabase()
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "views/index.html", gin.H{
